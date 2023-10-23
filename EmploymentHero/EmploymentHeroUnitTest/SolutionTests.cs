5403d6ec0a5a97e7e7911194897dd5a03e01aa4a
Dương Hoàng Minh <dhmin@netcompany.com>
2023-10-23 16:56:50 +0700

    Add code

--- EmploymentHero/EmploymentHeroUnitTest/SolutionTests.cs
+++ EmploymentHero/EmploymentHeroUnitTest/SolutionTests.cs
@@ -1,10 +1,40 @@
 ﻿using EmployementHeroTest;
+using System.Diagnostics;
+
 namespace EmploymentHeroUnitTest
 {
     [TestFixture]
     public class SolutionTests
     {
         Solution sol = new Solution();
+
+        [Test]
+        public void PerformanceTest()
+        {
+            // Generate a large number of connections
+            int n = 10000;
+            string[] Connections = new string[n];
+            for (int i = 0; i < n - 1; i++)
+            {
+                Connections[i] = $"{i}:{i + 1}";
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Execute the function
+            int result = sol.solution(Connections, "0", $"{n - 1}");
+
+            stopwatch.Stop();
+            long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+
+            // Check the result and print the time taken
+            Assert.That(result, Is.EqualTo(n - 1));
+            Console.WriteLine($"Time taken: {elapsedMilliseconds} ms");
+
+            // Optionally, you can assert that the time should be below a certain threshold
+            Assert.LessOrEqual(elapsedMilliseconds, 200);  // for example, less than 100 ms
+        }
 
         [TestCase(new string[] { "Martin:Job", "Kim:Job", "martin:David", "Kim:Larsey", "Larsey:Job" }, "Martin", "Job", 1)]
         [TestCase(new string[] { "Martin:Job", "Kim:Job", "Kim:David", "Kim:Larsey", "Larsey:Job", "Larsey:David" }, "martin", "David", 2)]
@@ -23,6 +53,7 @@ namespace EmploymentHeroUnitTest
         {
             Assert.That(sol.solution(Connections, Name1, Name2), Is.EqualTo(expected));
         }
+
     }
 
 }
