350771c19dcc8b554d4270a9ae7304bc90640abf
Dương Hoàng Minh <dhmin@netcompany.com>
2023-10-23 17:15:33 +0700

    Add code

---
+++ EmploymentHero/EmployementHeroTest/Test4.cs
@@ -0,0 +1,79 @@
+﻿namespace EmployementHeroTest
+{
+
+    public class InvoiceItem
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class Invoice
+    {
+        public Invoice()
+        {
+            InvoiceItems = new List<InvoiceItem>();
+        }
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Number { get; set; }
+        public string Seller { get; set; }
+        public string Buyer { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? AcceptanceDate { get; set; }
+        public IList<InvoiceItem> InvoiceItems { get; set; }
+    }
+
+    public interface IInvoiceRepository
+    {
+        decimal? GetTotal(int invoiceId);
+        decimal GetTotalOfUnpaid();
+        IReadOnlyDictionary<string, long> GetItemsReport(DateTime? from, DateTime? to);
+    }
+
+
+    public class InvoiceRepository : IInvoiceRepository
+    {
+        private readonly IQueryable<Invoice> _invoices;
+
+        public InvoiceRepository(IQueryable<Invoice> invoices)
+        {
+            _invoices = invoices;
+        }
+
+        /// <summary>
+        /// GetTotal Method
+        /// </summary>
+        /// <param name="invoiceId">The unique identifier of the invoice.</param>
+        /// <returns>Returns the total value of the invoice as a decimal?.
+        /// Returns null if the invoice with the given invoiceId does not exist.</returns>
+        public decimal? GetTotal(int invoiceId)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// GetTotalOfUnpaid Method
+        /// </summary>
+        /// <returns>Returns the total value of all unpaid invoices as a decimal.</returns>
+        public decimal GetTotalOfUnpaid()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// GetItemsReport Method
+        /// </summary>
+        /// <param name="from">The start date of the period (can be null).</param>
+        /// <param name="to">The end date of the period (can be null).</param>
+        /// <returns>Returns a dictionary where the key is the name of the invoice item and the value is the total number of items bought within the specified time period.</returns>
+        public IReadOnlyDictionary<string, long> GetItemsReport(DateTime? from, DateTime? to)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ EmploymentHero/EmploymentHeroUnitTest/InvoiceRepositoryTests.cs
@@ -0,0 +1,115 @@
+﻿using EmployementHeroTest;
+
+namespace EmploymentHeroUnitTest
+{
+
+
+    [TestFixture]
+    public class InvoiceRepositoryTests
+    {
+        private IQueryable<Invoice> _invoices;
+        private InvoiceRepository _repository;
+
+        [SetUp]
+        public void Setup()
+        {
+            _invoices = new List<Invoice>
+        {
+            new Invoice { Id = 1, Number = "INV001", Seller = "Seller1", Buyer = "Buyer1", CreationDate = DateTime.Parse("2021-01-01"), AcceptanceDate = DateTime.Parse("2021-01-10"), InvoiceItems = new List<InvoiceItem> { new InvoiceItem { Name = "Item1", Price = 10, Quantity = 1 }, new InvoiceItem { Name = "Item2", Price = 20, Quantity = 2 } } },
+            new Invoice { Id = 2, Number = "INV002", Seller = "Seller2", Buyer = "Buyer2", CreationDate = DateTime.Parse("2021-02-01"), InvoiceItems = new List<InvoiceItem> { new InvoiceItem { Name = "Item1", Price = 10, Quantity = 1 } } },
+            new Invoice { Id = 3, Number = "INV003", Seller = "Seller3", Buyer = "Buyer3", CreationDate = DateTime.Parse("2021-03-01"), AcceptanceDate = DateTime.Parse("2021-03-10"), InvoiceItems = new List<InvoiceItem> { new InvoiceItem { Name = "Item3", Price = 30, Quantity = 3 } } }
+        }.AsQueryable();
+
+            _repository = new InvoiceRepository(_invoices);
+        }
+
+        [Test]
+        public void GetTotal_ValidInvoiceId_ReturnsTotal()
+        {
+            var total = _repository.GetTotal(1);
+            Assert.That(total, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void GetTotal_InvalidInvoiceId_ReturnsNull()
+        {
+            var total = _repository.GetTotal(4);
+            Assert.IsNull(total);
+        }
+
+        [Test]
+        public void GetTotalOfUnpaid_ReturnsTotalOfUnpaidInvoices()
+        {
+            var total = _repository.GetTotalOfUnpaid();
+            Assert.That(total, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void GetItemsReport_ValidDateRange_ReturnsCorrectReport()
+        {
+            var report = _repository.GetItemsReport(DateTime.Parse("2021-01-01"), DateTime.Parse("2021-02-01"));
+            Assert.That(report.Count, Is.EqualTo(2));
+            Assert.That(report["Item1"], Is.EqualTo(2));
+            Assert.That(report["Item2"], Is.EqualTo(2));
+        }
+
+        [Test]
+        public void GetItemsReport_NullDateRange_ReturnsCorrectReport()
+        {
+            var report = _repository.GetItemsReport(null, null);
+            Assert.That(report.Count, Is.EqualTo(3));
+            Assert.That(report["Item1"], Is.EqualTo(2));
+            Assert.That(report["Item2"], Is.EqualTo(2));
+            Assert.That(report["Item3"], Is.EqualTo(3));
+        }
+
+        [Test]
+        public void GetTotal_EmptyInvoiceItems_ReturnsZero()
+        {
+            var total = _repository.GetTotal(4); // Assume Invoice with Id=4 has empty InvoiceItems
+            Assert.That(total, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetTotalOfUnpaid_NoUnpaidInvoices_ReturnsZero()
+        {
+            // Assume all invoices are paid in the setup
+            var total = _repository.GetTotalOfUnpaid();
+            Assert.That(total, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetItemsReport_EmptyDateRange_ReturnsEmptyReport()
+        {
+            var report = _repository.GetItemsReport(DateTime.Parse("2022-01-01"), DateTime.Parse("2022-01-01"));
+            Assert.IsEmpty(report);
+        }
+
+        [Test]
+        public void GetItemsReport_SingleDayRange_ReturnsCorrectReport()
+        {
+            var report = _repository.GetItemsReport(DateTime.Parse("2021-01-01"), DateTime.Parse("2021-01-01"));
+            Assert.That(report.Count, Is.EqualTo(2));
+            Assert.That(report["Item1"], Is.EqualTo(1));
+            Assert.That(report["Item2"], Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetItemsReport_NoInvoiceItems_ReturnsEmptyReport()
+        {
+            // Assume no invoice items in the setup
+            var report = _repository.GetItemsReport(null, null);
+            Assert.IsEmpty(report);
+        }
+
+        [Test]
+        public void GetItemsReport_MultipleInvoiceItemsSameName_ReturnsAggregatedCount()
+        {
+            // Assume multiple invoice items with the same name but different invoices
+            var report = _repository.GetItemsReport(null, null);
+            Assert.That(report.Count, Is.EqualTo(1));
+            Assert.That(report["Item1"], Is.EqualTo(6)); // 2 from Invoice 1, 2 from Invoice 2, 2 from Invoice 3
+        }
+    }
+
+}
